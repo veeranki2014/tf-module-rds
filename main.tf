4cd709d1af2ccc48d18b2fe264c92e48556a7a4f
veeranki2014 <veeranki2014@gmail.com>
2023-09-26 12:23:55 -0500

    terraform rabbitmq module

--- main.tf
+++ main.tf
@@ -14,4 +14,6 @@ resource "aws_rds_cluster" "main" {
   #db_name                 = var.db_name
   master_username         = data.aws_ssm_parameter.username.value
   master_password         = data.aws_ssm_parameter.password.value
+  db_subnet_group_name    = aws_db_subnet_group.main.name
+  kms_key_id              = var.kms_key_id
 }
--- variables.tf
+++ variables.tf
@@ -5,3 +5,4 @@ variable "engine_version" {}
 variable "subnet_ids" {}
 variable "tags" {}
 variable "db_name" {}
+variable "kms_key_id" {}
